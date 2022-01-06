97d68e73e722e5a06276bbb09a40a6c3cfd4c11a
shinaaaa <shin029abc@gmail.com>
2022-01-06 23:47:39 +0900

    feat: init project

---
+++ src/main/kotlin/com/shin/springbykotlin/SpringByKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.shin.springbykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringByKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringByKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/shin/springbykotlin/SpringByKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shin.springbykotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringByKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
